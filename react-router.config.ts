927b2860b156d968661a1834f2a8c952bd0cfa89
jayanthansenthilkumar <jayanthansenthilkumar18@gmail.com>
2026-07-01 22:10:40 +0530

    Revert GitHub Pages configuration

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  basename: "/jayanthan.fyi/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
 } satisfies Config;
